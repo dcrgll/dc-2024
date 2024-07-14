c848e5bfe2f8949f8ac3ca37345de8e58c76c298
Dan Cargill <dan@cargill.dev>
2024-07-14 19:06:16 +0100

    Add type safe env variables

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
+
+import { fileURLToPath } from 'node:url'
+import createJiti from 'jiti'
+
+const jiti = createJiti(fileURLToPath(import.meta.url))
+
+jiti('./src/env')
+
 const nextConfig = {}
 
 export default nextConfig
